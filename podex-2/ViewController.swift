f195130bc2f018d892b00d7d9155ee121cbeae5e
Hadi Asiaie <hadiasiaie@hadis-macbook.local>
2017-07-12 12:43:18 +0430

    Initial Commit

---
+++ podex-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  podex-2
+//
+//  Created by Hadi Asiaie on 7/12/17.
+//  Copyright © 2017 Hadi Asiaie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
